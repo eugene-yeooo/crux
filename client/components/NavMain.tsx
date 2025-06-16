ee6e08d13f87a2931e31b1e8148fbf9b5e4bd7c3
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-06-17 11:49:45 +1200

    add margin right to NavMain icons

--- client/components/NavMain.tsx
+++ client/components/NavMain.tsx
@@ -10,14 +10,15 @@ export default function NavMain() {
     }`;
 
     const iconSize = 21
+    const iconStyle = "mr-2"
 
   return (
     <nav className="bg-white p-0 rounded-lg shadow-lg w-full border">
       <div className="flex flex-col space-y-2">
-        <NavLink to="/log-nav" className={navLinkClass}><Send size={19}/> Log a send</NavLink>
-        <NavLink to="/user/eugeneyeooo" className={navLinkClass}><User size={iconSize} /> Profile</NavLink>
-        <NavLink to="/" className={navLinkClass}><LayoutDashboard size={iconSize} /> Dashboard</NavLink>
-        <NavLink to="/explore" className={navLinkClass}><MapPlus size={iconSize} /> Explore</NavLink>
+        <NavLink to="/log-nav" className={navLinkClass}><Send size={19} className={iconStyle} />Log a send</NavLink>
+        <NavLink to="/user/eugeneyeooo" className={navLinkClass}><User size={iconSize} className={iconStyle} />Profile</NavLink>
+        <NavLink to="/" className={navLinkClass}><LayoutDashboard size={iconSize} className={iconStyle} />Dashboard</NavLink>
+        <NavLink to="/explore" className={navLinkClass}><MapPlus size={iconSize} className={iconStyle} />Explore</NavLink>
       </div>
     </nav>
   );
