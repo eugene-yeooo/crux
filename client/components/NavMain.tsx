57eae6f6b53bb5339578fe5635011ee6498b372b
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-06-17 09:32:26 +1200

    add icons to NavMain

--- client/components/NavMain.tsx
+++ client/components/NavMain.tsx
@@ -1,21 +1,25 @@
 import { NavLink } from 'react-router';
+import { User, MapPlus, LayoutDashboard, Send } from 'lucide-react';
 
 export default function NavMain() {
   const navLinkClass = ({ isActive }: { isActive: boolean }) =>
     `transition-colors px-3 py-4 font-medium ${
       isActive
-        ? 'bg-brandPrimary text-brandBlack font-black'
-        : 'text-brandBlack hover:bg-brandBlack hover:text-white'
+        ? 'bg-brandPrimary text-brandBlack font-black inline-flex items-center gap-1'
+        : 'text-brandBlack hover:bg-brandBlack hover:text-white inline-flex items-center gap-1'
     }`;
+
+    const iconSize = 21
 
   return (
     <nav className="bg-white p-0 rounded-lg shadow-lg w-full border">
       <div className="flex flex-col space-y-2">
-        <NavLink to="/log-nav" className={navLinkClass}>Log a send</NavLink>
-        <NavLink to="/user/eugeneyeooo" className={navLinkClass}>Profile</NavLink>
-        <NavLink to="/" className={navLinkClass}>Dashboard</NavLink>
-        <NavLink to="/explore" className={navLinkClass}>Explore</NavLink>
+        <NavLink to="/log-nav" className={navLinkClass}><Send size={19}/> Log a send</NavLink>
+        <NavLink to="/user/eugeneyeooo" className={navLinkClass}><User size={iconSize} /> Profile</NavLink>
+        <NavLink to="/" className={navLinkClass}><LayoutDashboard size={iconSize} /> Dashboard</NavLink>
+        <NavLink to="/explore" className={navLinkClass}><MapPlus size={iconSize} /> Explore</NavLink>
       </div>
     </nav>
   );
 }
+ 
