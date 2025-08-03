0376c5a8281c99d1e16d2eb81dbc42aa3ede274f
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-08-03 15:32:37 +1200

    create climb-logs table

--- server/db/migrations/20250803031045_climb-logs.js
+++ server/db/migrations/20250803031045_climb-logs.js
@@ -2,14 +2,30 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export async function up(knex) {
+  return knex.schema.createTable('log_climbs', (table) => {
+    table
+      .integer('log_id')
+      .unsigned()
+      .notNullable()
+      .primary()
+      .references('id')
+      .inTable('logs')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+    table.string('grade')
+    table.string('send')
+    table.string('route_style')
+    table.integer('height')
+    table.integer('attempts')
+    table.string('team')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export async function down(knex) {
+  return knex.schema.dropTableIfExists('log_climbs')
+}
