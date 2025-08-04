20254f19ec375dc8b4d8d37db981c95c01dc6d27
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-08-04 18:49:51 +1200

    move profile stats into profile details component; style stat boxes

--- client/components/profile page/ProfileSummaryStats.tsx
+++ client/components/profile page/ProfileSummaryStats.tsx
@@ -5,17 +5,17 @@ export default function LogSummaryStats({
 }: {
   logs: Log[]
 }) {
-  const total = logs.length
-  const canyons = logs.filter((l) => l.type === 'canyon')
-  const climbs = logs.filter((l) => l.type === 'climb')
-  const hardest =
-    climbs
-      .map((c) => c.grade || '')
-      .sort()
-      .slice(-1)[0] || 'N/A'
+  // const total = logs.length
+  // const canyons = logs.filter((l) => l.type === 'canyon')
+  // const climbs = logs.filter((l) => l.type === 'climb')
+  // const hardest =
+  //   climbs
+  //     .map((c) => c.grade || '')
+  //     .sort()
+  //     .slice(-1)[0] || 'N/A'
 
   return (
-    <div className="p- max-w-4xl mx-auto mb-6">
+    <div>
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
         <StatBox label="Total Sends" value="869" />
         <StatBox label="Distance Climbed" value="12.6km" />
@@ -28,7 +28,7 @@ export default function LogSummaryStats({
 
 function StatBox({ label, value }: { label: string; value: string }) {
   return (
-    <div className="bg-white rounded-lg p-6 text-center shadow-lg">
+    <div className="bg-white rounded-lg p-6 text-center shadow-lg border border-gray-500 px-7">
       <div className="text-3xl font-bold text-brandBlack pb-2">{value}</div>
       <div className="text-sm text-gray-700 font-semibold">{label}</div>
     </div>
--- client/components/profile page/UserProfilePage.tsx
+++ client/components/profile page/UserProfilePage.tsx
@@ -39,8 +39,11 @@ export default function UserProfilePage() {
 
   return (
     <div>
-      {user && <ProfileDetails user={user} />}
-      <ProfileSummaryStats logs={logs} />
+      
+      {user && <ProfileDetails user={user} logs={logs} />}
+      
+      {/* <ProfileSummaryStats logs={logs} /> */}
+      
       {/* <NotableSends logs={logs} /> */}
       <NavProfileFilter selected={filter} onChange={setFilter} />
       {/* <div className="bg-white shadow-lg rounded-lg p-6 max-w-4xl mx-auto my-4">
