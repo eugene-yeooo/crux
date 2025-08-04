7bb9e87ca38bc7c73adbf0a373f2dd568d331a4a
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-08-04 18:13:31 +1200

    tweak styling for no logs yet

--- client/components/profile page/ProfileLogList.tsx
+++ client/components/profile page/ProfileLogList.tsx
@@ -4,8 +4,8 @@ import LogCard from '../LogCard'
 export default function LogList({ logs }: { logs: Log[] }) {
   if (logs.length === 0) {
     return (
-      <div className="bg-white shadow-lg rounded-lg p-6 max-w-4xl mx-auto my-2">
-        <p className="text-center text-gray-500">No logs yet.</p>
+      <div className="bg-white shadow-lg rounded-lg p-6 max-w-6xl mx-auto my-2">
+        <p className="text-center text-gray-500 my-16">No logs yet.</p>
       </div>
     )
   }
