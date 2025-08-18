23f04bc10ed51840683ee2f8a322543a3e4d7dfb
Eugene Yeo <eugeneyeoooo@gmail.com>
2025-08-18 18:16:21 +1200

    implement radix-ui dropdown on logpage

--- client/components/log page/DropdownLogPage.tsx
+++ client/components/log page/DropdownLogPage.tsx
@@ -1,6 +1,6 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { Link } from 'react-router'
-import { Edit3, Trash2 } from 'lucide-react'
+import { Edit3, Trash2, Pencil } from 'lucide-react'
 import { forwardRef } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
@@ -15,20 +15,59 @@ function LogDropdownMenu({ logId, logType, onInitDelete }: Props, ref: React.Ref
   const { user } = useAuth0()
   
   return (
-    <div ref={ref} className="absolute top-6 left-64 bg-white border shadow rounded w-40 py-2">
-      <Link
+    // <div ref={ref} className="absolute top-6 left-64 bg-white border shadow rounded w-40 py-2">
+    //   <Link
+    //     to={`/user/${user?.nickname}/log/${logId}/edit-${logType}`}
+    //     className="flex items-center px-4 py-2 text-sm hover:bg-gray-100"
+    //   >
+    //     <Edit3 size={16} className="mr-2" /> Edit Log
+    //   </Link>
+    //   <button
+    //     className="flex items-center px-4 py-2 text-sm text-red-600 hover:bg-red-50 w-full text-left"
+    //     onClick={onInitDelete}
+    //   >
+    //     <Trash2 size={16} className="mr-2" /> Delete
+    //   </button>
+    // </div>
+
+
+<DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button
+          className="p-1 rounded-md text-brandPrimary hover:bg-gray-100 focus:outline-none"
+          aria-label="Open menu"
+        >
+          <Pencil size={20} className="text-gray-400 hover:text-black" />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className="z-50 w-40 rounded border bg-white py-2 shadow-md"
+          sideOffset={32.5}
+          align="end"
+        >
+          <DropdownMenu.Item asChild>
+            <Link
         to={`/user/${user?.nickname}/log/${logId}/edit-${logType}`}
         className="flex items-center px-4 py-2 text-sm hover:bg-gray-100"
       >
         <Edit3 size={16} className="mr-2" /> Edit Log
       </Link>
-      <button
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item
+          >
+<button
         className="flex items-center px-4 py-2 text-sm text-red-600 hover:bg-red-50 w-full text-left"
         onClick={onInitDelete}
       >
         <Trash2 size={16} className="mr-2" /> Delete
       </button>
-    </div>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   )
 }
 
